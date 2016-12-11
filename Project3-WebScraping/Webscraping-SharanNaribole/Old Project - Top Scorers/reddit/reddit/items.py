1ef0dc4e621a82312270928d6538c260786d7011
sharan-naribole <sn23@rice.edu>
2016-12-11 13:26:14 -0600

    Completed transport of old project

---
+++ Project3-WebScraping/Webscraping-SharanNaribole/Old Project - Top Scorers/reddit/reddit/items.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class RedditItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    title = scrapy.Field()
+    link = scrapy.Field()
+    comments = scrapy.Field()
+    time = scrapy.Field()
+    score = scrapy.Field()
+    
