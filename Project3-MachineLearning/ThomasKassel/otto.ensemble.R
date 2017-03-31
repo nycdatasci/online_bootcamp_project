a9ad2466f2aa7c7b6d46f779329e4bbc7c74d3dd
thomas-kassel <tkassel91@gmail.com>
2017-03-30 23:30:00 -0400

    Clean up scriptso it only contains AvgModels function

--- Project3-MachineLearning/ThomasKassel/otto.ensemble.R
+++ Project3-MachineLearning/ThomasKassel/otto.ensemble.R
@@ -1,6 +1,5 @@
-# Functions to help with ensembling and diagnostics of Otto predictions
+# Function to blend multiple model outputs for Otto classification
 
-library(MLmetrics)
 library(data.table)
 
 AvgModels <- function(probabilityDFs,model.weights){
@@ -14,17 +13,3 @@ AvgModels <- function(probabilityDFs,model.weights){
   averaged <- combined[,lapply(.SD,weighted.mean,w = model.weights),by = id]
   return(averaged)
 }
-
-# Example usage - read in the 30% test predictions for each of three h2o models (glm,randomforest,gbm)
-glmPred <- fread('./modelOutputs/glm.predictions.csv',colClasses = c(id="character"))
-rfPred <- fread('./modelOutputs/rf.predictions.csv',colClasses = c(id="character"))
-gbmPred <- fread('./modelOutputs/gbm.predictions.csv',colClasses = c(id="character"))
-# Actual class assignments of 30% test split
-trueClasses <- fread('./modelOutputs/testing.outcomes.csv',col.names = c("id","actual"))
-
-# Average probability over the three models for each observation, for each class
-averagePred <- AvgModels(list(glmPred,rfPred,gbmPred),c(1,1,10))
-fwrite(x = averagePred,file = 'AveragedPredictions.csv',row.names = F)
-
-# Calculate multi-class log loss over the new averaged predictions
-MultiLogLoss(y_pred = as.matrix(averagePred[,-"id"]), y_true = as.vector(trueClasses$actual))
